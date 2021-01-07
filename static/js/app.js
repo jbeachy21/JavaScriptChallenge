0dacb73cafe73fd07feed3f3a844e35097dbdfb6
Jasper Beachy <jbeach17@gmail.com>
2021-01-07 15:09:51 -0600

    changes made

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,32 @@
 // from data.js
 var tableData = data;
 
+
+function populateTable() {
+    var size = tableData.length;
+
+
+console.log(tableData[0].datetime);
 // YOUR CODE HERE!
+var tbody = d3.select("tbody")
+var thead = d3.select("thead");
+var table = d3.select("#ufo-table");
+table.attr("class", "table table-responsive table-hover table-striped");
+
+
+for (var i = 0; i<size; i++) {
+    var tr = tbody.append("tr");
+    tr.attr("class", "text-center");
+    tr.append("td").text(tableData[i].datetime);
+    tr.append("td").text(tableData[i].city);
+    tr.append("td").text(tableData[i].state);
+    tr.append("td").text(tableData[i].country);
+    tr.append("td").text(tableData[i].shape);
+    tr.append("td").text(tableData[i].durationMinutes);  
+    tr.append("td").text(tableData[i].comments);    
+    }
+
+
+}
+
+populateTable();
